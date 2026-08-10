1838daad15c9838895482e19b476a2fba4fcfb87
Lueur <sgdm515@gmail.com>
2026-08-10 19:17:36 +0300

    Read source list from TOML config

--- api_collector/config.py
+++ api_collector/config.py
@@ -1,5 +1,15 @@
-import sys
+from pathlib import Path
+import tomllib
+from typing import Any
 
 
-def foo():
-    return sys.path[0]
+def read_config(config_path: Path) -> dict[str, dict[str, Any]]:
+    api_urls: dict[str, dict[str, Any]] = {}
+    with open(config_path, 'rb') as f:    
+        config = tomllib.load(f)
+        for item in config['API']:
+            name = item['name']
+
+            api_urls[name] = {'URL': item['URL'], 'timeout': item.get('timeout', 5)}
+    return api_urls
+
--- api_collector/main.py
+++ api_collector/main.py
@@ -1,8 +1,16 @@
-from api_collector.config import foo
+from pathlib import Path
+from api_collector.config import read_config 
+from api_collector.client import get_request 
+
+MODULE_DIR = Path(__file__).parent
+API_CONFIG = MODULE_DIR.parent / 'config.toml'
 
 
-def main():
-    print(foo())
+
+def main() -> None:
+    api_config = read_config(API_CONFIG)
+    
+    
 
 if __name__ == "__main__":
     main()
