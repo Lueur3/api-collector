90d4b2b24139e000d069b4edfd7a4c097e03992c
Lueur <sgdm515@gmail.com>
2026-08-24 14:47:49 +0300

    Flatten error handling in client

--- api_collector/client.py
+++ api_collector/client.py
@@ -7,6 +7,8 @@ MAX_LEN_RESPONSE = 2000
 
 
 def truncate_response(raw_text: str) -> str:
+    if len(raw_text) <= MAX_LEN_RESPONSE:
+        return raw_text
     slice_length = MAX_LEN_RESPONSE // 2
     raw_data = (
         raw_text[:slice_length]
@@ -17,45 +19,53 @@ def truncate_response(raw_text: str) -> str:
     return raw_data
 
 
+def get_data(response: requests.models.Response) -> dict[str, Any] | str:
+    raw_data: dict[str, Any] | str
+    try:
+        raw_data = response.json()
+    except requests.exceptions.JSONDecodeError:
+        raw_data = truncate_response(response.text)
+
+    return raw_data
+
+
+@retry()
 def get_request(api_source: Source) -> SourceResponse:
+    try:
+        response = requests.get(api_source.url, timeout=api_source.timeout)
+    except requests.exceptions.Timeout as e:
+        raise exceptions.NetworkTimeoutError(
+            f"Timeout during loading '{api_source.url}'"
+        ) from e
+    except requests.exceptions.ConnectionError as e:
+        raise exceptions.NetworkConnectionError(
+            f"Unable to connect to '{api_source.url}'"
+        ) from e
+    except requests.RequestException as e:
+        raise exceptions.NetworkError("Unexpected network error.") from e
 
     try:
-        try:
-            response = requests.get(api_source.url, timeout=api_source.timeout)
-        except requests.exceptions.Timeout as e:
-            raise exceptions.NetworkTimeoutError(
-                f"Timeout during loading '{api_source.url}'"
-            ) from e
-        except requests.exceptions.ConnectionError as e:
-            raise exceptions.NetworkConnectionError(
-                f"Unable to connect to '{api_source.url}'"
-            ) from e
+        response.raise_for_status()
+        data_response = response.json()
+    except requests.exceptions.HTTPError as e:
+        raw_data = get_data(response)
 
-        try:
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            try:
-                raw_data = response.json()
-            except requests.exceptions.JSONDecodeError:
-                raw_data = response.text
-                if len(raw_data) > MAX_LEN_RESPONSE:
-                    raw_data = truncate_response(raw_data)
-
+        if response.status_code in RETRY_CODES:
+            raise exceptions.RetryHttpError(
+                status_code=response.status_code, raw=raw_data
+            ) from e
+        else:
             raise exceptions.NetworkHttpError(
                 status_code=response.status_code, raw=raw_data
             ) from e
 
-        try:
-            data_response = response.json()
-        except requests.exceptions.JSONDecodeError as e:
-            raw_data = response.text
-            if len(raw_data) > MAX_LEN_RESPONSE:
-                raw_data = truncate_response(raw_data)
-            raise exceptions.RequestError(
-                message="An unsuitable answer option has been received.",
-                status_code=response.status_code,
-                raw=raw_data,
-            ) from e
+    except requests.exceptions.JSONDecodeError as e:
+        raw_data = truncate_response(response.text)
+        raise exceptions.RequestError(
+            message="An unsuitable answer option has been received.",
+            status_code=response.status_code,
+            raw=raw_data,
+        ) from e
 
     except requests.RequestException as e:
         raise exceptions.NetworkError("Unexpected network error.") from e
